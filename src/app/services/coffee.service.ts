556c057924b30b607153383d2f19819b5fe93333
Renan <renan_cavalcanti@hotmail.com>
2025-06-11 12:52:26 -0400

    implement service

--- src/app/services/coffee.service.ts
+++ src/app/services/coffee.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Coffee{
+  id: number;
+  title: string;
+  description: string;
+  ingredients: string[];
+  image: string;
+  showDetails: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoffeeService {
 
-  constructor() { }
+  private iceUrl = "https://api.sampleapis.com/coffee/iced"
+  private hotUrl = "https://api.sampleapis.com/coffee/hot"
+
+  constructor(private http: HttpClient) { }
+
+  getIcedCoffee(): Observable<Coffee[]> {
+    return this.http.get<Coffee[]>(this.iceUrl).pipe()
+  }
+
+  getHotCoffee(): Observable<Coffee[]> {
+    return this.http.get<Coffee[]>(this.hotUrl).pipe()
+  }
+
 }
