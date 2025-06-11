b21b13789c9ee10fe9efbe8f08d1ea524f51b7c2
Renan <renan_cavalcanti@hotmail.com>
2025-06-11 13:20:02 -0400

    implement iced coffee component

--- src/app/components/iced-coffee/iced-coffee.component.ts
+++ src/app/components/iced-coffee/iced-coffee.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Coffee, CoffeeService } from '../../services/coffee.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-iced-coffee',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './iced-coffee.component.html',
   styleUrl: './iced-coffee.component.scss'
 })
 export class IcedCoffeeComponent {
 
+  coffees: Coffee[] = []
+
+  constructor(private service: CoffeeService){
+    this.service.getIcedCoffee().subscribe({
+      next: (list) => {
+        this.coffees = list.map(c => (
+          {...c, showDetails: false}))
+        console.log(this.coffees)
+      },
+      error: () => {}
+    })
+  }
+
+  toggleDetails(id: number) {
+    console.log(id)
+    this.coffees = this.coffees.map( c => 
+      c.id === id ? {...c, showDetails: !c.showDetails} : c
+    )
+    console.log(this.coffees)
+  }
 }
