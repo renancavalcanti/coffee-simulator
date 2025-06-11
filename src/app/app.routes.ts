846cb216eb374d4a79beaaeb89f80b6e7ce4263f
Renan <renan_cavalcanti@hotmail.com>
2025-06-11 12:43:09 -0400

    create navbar and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { IcedCoffeeComponent } from './components/iced-coffee/iced-coffee.component';
+import { HotCoffeeComponent } from './components/hot-coffee/hot-coffee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: IcedCoffeeComponent},
+    {path: 'iced', component: IcedCoffeeComponent},
+    {path: 'hot', component: HotCoffeeComponent},
+];
