2ada14be384abe17b2aefc1df926b06726b26e27
Renan <renan_cavalcanti@hotmail.com>
2025-06-11 13:26:00 -0400

    implement hot coffee component

--- src/app/components/hot-coffee/hot-coffee.component.ts
+++ src/app/components/hot-coffee/hot-coffee.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Coffee, CoffeeService } from '../../services/coffee.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hot-coffee',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hot-coffee.component.html',
   styleUrl: './hot-coffee.component.scss'
 })
 export class HotCoffeeComponent {
+  coffees: Coffee[] = []
 
+  constructor(private service: CoffeeService){
+    this.service.getHotCoffee().subscribe({
+      next: (list) => {
+        this.coffees = list.map(c => (
+          {...c, showDetails: false}))
+        console.log(this.coffees)
+      },
+      error: () => {}
+    })
+  }
+
+  toggleDetails(id: number) {
+    console.log(id)
+    this.coffees = this.coffees.map( c => 
+      c.id === id ? {...c, showDetails: !c.showDetails} : c
+    )
+    console.log(this.coffees)
+  }
 }
